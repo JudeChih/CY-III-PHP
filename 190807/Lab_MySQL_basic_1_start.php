61ea65f2bf3f84c268f8528a0badae173019374a
JudeChih <sausage760703@gmail.com>
2019-08-07 11:08:58 +0800

    201908071108

--- 190807/Lab_MySQL_basic_1_start.php
+++ 190807/Lab_MySQL_basic_1_start.php
@@ -5,24 +5,27 @@ header("content-type:text/html; charset=utf-8");
 
 
 // 1. 連接資料庫伺服器
-// $link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
-// $result = mysqli_query($link, "set names utf8");
-// mysqli_select_db($link, "class");
+
+$link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+// var_dump($link);
+$result = mysqli_query($link, "set names utf8");
+mysqli_select_db($link, "class");
 
 // 2. 執行 SQL 敘述
-// $commandText = "select * from students";
-// $result = mysqli_query($link, $commandText);
+$commandText = "select * from students";
+$result = mysqli_query($link, $commandText);
 
 // 3. 處理查詢結果
-// while ($row = mysqli_fetch_assoc($result))
-// {
-//   echo "ID：{$row['cID']}<br>";
-//   echo "Name：{$row['cName']}<br>";
-//   echo "<HR>";
-// }
+while ($row = mysqli_fetch_assoc($result))
+{
+  echo "ID：{$row['cID']}<br>";
+  echo "Name：{$row['cName']}<br>";
+  echo "Address：{$row['cAddr']}<br>";
+  echo "<HR>";
+}
 
 // 4. 結束連線
-// mysqli_close($link);
+mysqli_close($link);
 
 echo "<br />-- Done --";
 ?>
